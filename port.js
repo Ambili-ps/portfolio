9f670bb5da6ce6a22c920bf87cb580c7f2326b13
Ambili Suresh <ambilisuresh2003@gmail.com>
2025-05-04 16:46:02 +0530

    finalcommit

---
+++ port.js
@@ -0,0 +1,25 @@
+function sendMail(){
+    let params={
+        name:document.getElementById("name").value,
+        namee:document.getElementById("namee").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+        
+        
+    };
+    console.log(params);
+        const serviceID="service_8y0ijtw";
+const templateID="template_um3q7x6";
+emailjs.send(serviceID,templateID,params)
+.then( 
+    res=>{
+    document.getElementById("name").value="";
+    document.getElementById("namee").value="";
+    document.getElementById("email").value="";
+    document.getElementById("message").value="";
+    console.log(res)
+    alert("you message send successfully")
+
+    })
+Cache(err=>console.log(err));  
+}
